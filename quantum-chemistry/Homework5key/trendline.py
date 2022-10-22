00b2c05e4fd50c73a1d999416905e60e743de0af
Lexin Chen <110304515+lexinsea@users.noreply.github.com>
2022-10-22 12:22:04 -0400

    Update trendline.py

--- quantum-chemistry/Homework5key/trendline.py
+++ quantum-chemistry/Homework5key/trendline.py
@@ -1,17 +1,37 @@
 import matplotlib.pyplot as plt
-
 import numpy as np
- 
 import math
 
-x1 = np.linspace(0,1,300)
-y1=2*np.sin(10*math.pi*x1)**2
-f2 = interp1d(x1, y1, kind = 'cubic')
-y2=1
+#importing datapoints
+p=[1,3,5,7]
+p3=[4,6,8,10]
+lamb=[5.22e-07, 6.02e-7, 7.01e-7, 8.03e-7]
 
-plt.plot(x1, y1, c ='blue',label='n=10')
-plt.xlabel("Position (x/L)")
-plt.ylabel('Probability Distribution ' r'$(\psi^2/L)$')
-plt.axhline(y=1, color='r', linestyle='--',label='classical')
-plt.legend()
+#importing constants
+c = 3e8
+m = 9.109e-31
+h=6.626e-34
+
+#solving for the equation
+result=[]
+for i in range(0, len(lamb)):
+    result.append(math.sqrt(lamb[i] * (p[i]+4))/math.sqrt(8*c*m/h))
+
+#plotting the points on scatter plot
+plt.scatter(p3, result, c ="blue")
+  
+# naming the x axis
+plt.xlabel('p+3')
+# naming the y axis
+plt.ylabel(r'$\sqrt{λ(p+4)}/\sqrt{\frac{8cm_e}{h}}}$') #using latex 
+  
+#adding trendline
+z = np.polyfit(p3, result, 1)
+p = np.poly1d(z)
+
+# function to show the plot
+plt.plot(p3, p(p3),"r--")
+
+# giving a title to my graph
+plt.title("y={:0.3e}x+{:0.3e}".format(z[0],z[1])) 
 plt.show()
