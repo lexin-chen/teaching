06ac71d8432770a5716a0366f4b3c8f6d6f77332
Lexin Chen <110304515+lexinsea@users.noreply.github.com>
2022-10-24 11:12:01 -0400

    Update sin.py

--- quantum-chemistry/Homework6key/sin.py
+++ quantum-chemistry/Homework6key/sin.py
@@ -5,9 +5,9 @@ import math
 # Defining my interval 
 x1 = np.linspace(0,1,300) #(start,stop,offset); the large amount of steps will give that smoothened line
 # Defining the function to solve for that interval
-y1=2*np.sin(10*math.pi*x1)**2 
+y1 = 2 * np.sin(10 * math.pi * x1)**2 
 # Defining function #2
-y2=1
+y2 = 1
 #Plotting the graph
 plt.plot(x1, y1, c ='blue',label='n=10')
 plt.xlabel("Position (x/L)")
